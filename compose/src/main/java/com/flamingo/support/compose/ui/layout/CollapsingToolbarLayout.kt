bcf6ed0e4d12455873c1186715e5f6a0fe7a97e2
jhonboy121 <alfredmathew05@gmail.com>
2022-07-14 22:24:58 +0530

    CollapsingToolbarLayout: keep the top app bar below statusbar insets
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- compose/src/main/java/com/flamingo/support/compose/ui/layout/CollapsingToolbarLayout.kt
+++ compose/src/main/java/com/flamingo/support/compose/ui/layout/CollapsingToolbarLayout.kt
@@ -26,6 +26,7 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.material.icons.Icons
@@ -71,6 +72,7 @@ fun CollapsingToolbarLayout(
             decayAnimationSpec = rememberSplineBasedDecay()
         )
         LargeTopAppBar(
+            modifier = Modifier.statusBarsPadding(),
             title = {
                 Text(text = title, modifier = Modifier.padding(start = 4.dp))
             },
